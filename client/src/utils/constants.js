419e795c4384f3fdf3f3e5a6080d938135ca47d8
DinhHoang0607 <ngdhoang0607@gmail.com>
2022-12-01 17:28:39 +0700

    smart_contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress =
+	'0x3055668c4311A315eD6e2417ccdaf688205Cc868'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/K-V2upZZPV-4Nj7ZSfYMzAF3lvuWun2r
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.17",
+	solidity: '0.8.0',
+	networks: {
+		goerli: {
+			url: 'https://eth-goerli.g.alchemy.com/v2/K-V2upZZPV-4Nj7ZSfYMzAF3lvuWun2r',
+			accounts: [
+				'255189f230e05b90b8faea9ad80d3e8be2f85605af9d0bfdf1cfacc5db80dbf0',
+			],
+		},
+	},
 };
